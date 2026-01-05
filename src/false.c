6600b10691b67451dc24ad7c4895200487d6b30f
Ali Hamza <ali.mhamza06@gmail.com>
2026-01-05 21:57:10 +0400

    Implement 'true' and 'false', and update .gitignore.

---
+++ src/false.c
@@ -0,0 +1,55 @@
+/* Includes. */
+
+#include "../include/common.h"
+#include "../include/utils.h"
+#include <stddef.h>
+#include <string.h>
+
+/* Defines. */
+
+#define UTIL_NAME "false"
+
+/* Help option data. */
+
+static const opt_group
+false_options[] = {
+    {NULL, NULL, NULL}
+};
+
+static const char*
+false_option_details[] = {
+    NULL
+};
+
+static const char*
+false_notes[] = {
+    NULL
+};
+
+/* Helpers. */
+
+static void
+help_option()
+{
+    help_info info = {
+        "true",
+        "Exit with status code 1 (always).",
+        false_options,
+        false_option_details,
+        false_notes
+    };
+
+    display_help(&info);
+}
+
+/* Main. */
+
+int
+main(int argc, char *argv[])
+{
+    if ((argc == 2) && !strcmp(argv[1], "--help"))
+        help_option();
+    else if ((argc == 2) && !strcmp(argv[1], "--version"))
+        display_version(UTIL_NAME);
+    return 1;
+}
---
+++ src/true.c
@@ -0,0 +1,55 @@
+/* Includes. */
+
+#include "../include/common.h"
+#include "../include/utils.h"
+#include <stddef.h>
+#include <string.h>
+
+/* Defines. */
+
+#define UTIL_NAME "true"
+
+/* Help option data. */
+
+static const opt_group
+true_options[] = {
+    {NULL, NULL, NULL}
+};
+
+static const char*
+true_option_details[] = {
+    NULL
+};
+
+static const char*
+true_notes[] = {
+    NULL
+};
+
+/* Helpers. */
+
+static void
+help_option()
+{
+    help_info info = {
+        "true",
+        "Exit with status code 0 (always).",
+        true_options,
+        true_option_details,
+        true_notes
+    };
+
+    display_help(&info);
+}
+
+/* Main. */
+
+int
+main(int argc, char *argv[])
+{
+    if ((argc == 2) && !strcmp(argv[1], "--help"))
+        help_option();
+    else if ((argc == 2) && !strcmp(argv[1], "--version"))
+        display_version(UTIL_NAME);
+    return 0;
+}
