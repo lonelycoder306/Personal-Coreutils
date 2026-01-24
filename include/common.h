818ffa6ded113e80fd51cbcebac5a3cf40e50eb9
Ali Hamza <ali.mhamza06@gmail.com>
2026-01-24 04:36:10 +0400

    Add useful fixed-size integer macros.

--- include/common.h
+++ include/common.h
@@ -8,6 +8,18 @@
 
 #define VERSION_STR "0.0.1"
 
+#include <stdint.h>
+
+typedef int8_t      i8;
+typedef int16_t     i16;
+typedef int32_t     i32;
+typedef int64_t     i64;
+
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t    u64;
+
 typedef struct cl_opt {
     char        main;
     const char* alt;
